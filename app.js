0d648a65b613a578ec4b7b10fed48aaeacc919d6
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-03-23 12:58:21 +0100

    Template Engine and View Set up

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mainRoutes = require('./server/routes/routes');
 
+const expressLayout = require('express-ejs-layouts');
+
 dotenv.config();
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+
+//Template Engines
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 
 app.use('', mainRoutes);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,5 +5,5 @@
  */
 
 module.exports.get_home_page = (req, res) => {
-	res.send('We are on...');
+	res.render('home');
 }
