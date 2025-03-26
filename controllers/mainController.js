c12cb0e52a560b2cd8643ac5f3cf15efff84aa5d
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-03-26 22:46:14 +0100

    About Page

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -7,3 +7,14 @@
 module.exports.get_home_page = (req, res) => {
 	res.render('home');
 }
+
+
+/**
+ * GET /
+ * Main - About Page
+ */
+
+module.exports.get_about = (req, res) => {
+	res.render('about');
+}
+
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controllers = require('../../controllers/mainController');
 
-
 router.get('/', controllers.get_home_page);
+router.get('/about', controllers.get_about);
 
 module.exports = router;
