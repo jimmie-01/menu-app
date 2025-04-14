eb3ae92d47048aba4224521d8b01f5005ba600bd
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-04-14 12:45:35 +0100

    menu schema

---
+++ server/models/menu.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema({
+	section: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: 
+	}
+})
